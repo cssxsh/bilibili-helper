2f4977808bfe7bfb2bb88ce1bec88b8e0adf455c
cssxsh <cssxsh@gmail.com>
2020-10-27 06:01:45 +0800

    debug: AutoSavePluginConfig

--- src/main/kotlin/xyz/cssxsh/mirai/plugin/data/BilibiliTaskData.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/plugin/data/BilibiliTaskData.kt
@@ -1,12 +1,12 @@
 package xyz.cssxsh.mirai.plugin.data
 
 import kotlinx.serialization.Serializable
-import net.mamoe.mirai.console.data.AutoSavePluginData
+import net.mamoe.mirai.console.data.AutoSavePluginConfig
 import net.mamoe.mirai.console.data.value
 import net.mamoe.mirai.utils.minutesToMillis
 import java.util.*
 
-object BilibiliTaskData : AutoSavePluginData("BilibiliTaskData") {
+object BilibiliTaskData : AutoSavePluginConfig("BilibiliTaskData") {
     val video: MutableMap<Long, TaskInfo> by value()
 
     val live: MutableMap<Long, TaskInfo> by value()
