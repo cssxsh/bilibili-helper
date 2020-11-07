1edd2525e92c27cdab3f3c8a6972f346e52f0032
cssxsh <cssxsh@gmail.com>
2020-11-08 01:12:01 +0800

    fix: driver

--- src/main/kotlin/xyz/cssxsh/mirai/plugin/tools/ScreenShotTool.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/plugin/tools/ScreenShotTool.kt
@@ -3,28 +3,34 @@ package xyz.cssxsh.mirai.plugin.tools
 import kotlinx.coroutines.delay
 import org.openqa.selenium.OutputType
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeDriverService
 import org.openqa.selenium.chrome.ChromeOptions
+import java.io.File
 
-class ScreenShotTool(driverPath: String, chromePath: String? = null, deviceName: String? = null) {
-
-    init {
-        System.setProperty("webdriver.chrome.driver", driverPath)
+class ScreenShotTool(val driverPath: String, chromePath: String? = null, deviceName: String? = null) {
+    private val driver by lazy {
+        ChromeDriver(
+            ChromeDriverService.Builder().apply {
+                usingDriverExecutable(File(driverPath))
+                withVerbose(false)
+                withSilent(true)
+            }.build(),
+            ChromeOptions().apply {
+                addArguments("--headless")
+                chromePath.takeUnless {
+                    it.isNullOrEmpty()
+                }?.let {
+                    setBinary(it)
+                }
+                deviceName.takeUnless {
+                    it.isNullOrEmpty()
+                }?.let {
+                    setExperimentalOption("mobileEmulation", mapOf("deviceName" to deviceName))
+                }
+                setExperimentalOption("excludeSwitches", listOf("enable-logging"))
+            }
+        )
     }
-
-    private val driver = ChromeDriver(ChromeOptions().apply {
-        addArguments("--headless")
-        chromePath.takeUnless {
-            it.isNullOrEmpty()
-        }?.let {
-            setBinary(it)
-        }
-        deviceName.takeUnless {
-            it.isNullOrEmpty()
-        }?.let {
-            setExperimentalOption("mobileEmulation", mapOf("deviceName" to deviceName))
-        }
-        setExperimentalOption("excludeSwitches", listOf("enable-logging"))
-    })
 
     suspend fun getScreenShot(url: String, delayMillis: Long = 10_000): ByteArray = driver.run {
         manage().window()
