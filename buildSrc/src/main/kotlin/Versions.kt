759c97ca428d5ba8c8cdc2022673f34157360f75
cssxsh <cssxsh@gmail.com>
2020-11-08 05:24:44 +0800

    fix: Versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,6 +3,7 @@ object Versions {
     // mirai
     const val core = "1.3.2"
     const val console = "1.0-RC-1"
+    const val autoService = "1.0-rc7"
     // kotlin
     const val kotlin = "1.4.10"
     const val serialization = "1.0-M1-1.4.0-rc"
@@ -10,8 +11,5 @@ object Versions {
     // other
     const val shadow = "6.1.0"
     const val junit = "5.6.2"
-    const val korlibs = "1.12.0"
-    const val autoService = "1.0-rc7"
-    const val okhttp = "4.9.0"
     const val selenium = "3.141.59"
 }
