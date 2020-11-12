327689f5a55b2095cca3c2a8fab4fd9eb3fb8783
cssxsh <cssxsh@gmail.com>
2020-11-12 13:33:08 +0800

    debug:

--- src/test/kotlin/mirai/RunMirai.kt
+++ src/test/kotlin/mirai/RunMirai.kt
@@ -1,16 +1,16 @@
 package mirai
 
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import mirai.command.StateCommand
+import net.mamoe.mirai.Bot
 import net.mamoe.mirai.console.MiraiConsole
+import net.mamoe.mirai.console.MiraiConsole.INSTANCE.isActive
 import net.mamoe.mirai.console.command.CommandManager.INSTANCE.register
 import net.mamoe.mirai.console.terminal.ConsoleTerminalExperimentalApi
 import net.mamoe.mirai.console.terminal.MiraiConsoleImplementationTerminal
 import net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader
 import net.mamoe.mirai.console.util.ConsoleExperimentalApi
+import net.mamoe.mirai.contact.Friend
 import net.mamoe.mirai.event.events.BotOnlineEvent
 import net.mamoe.mirai.event.subscribeAlways
 import net.mamoe.mirai.utils.minutesToMillis
@@ -33,27 +33,26 @@ object RunMirai {
     @JvmStatic
     fun main(args: Array<String>): Unit = runBlocking {
         // 默认在 /test 目录下运行
+        MiraiConsoleTerminalLoader.parse(args, exitProcess = true)
+        MiraiConsoleTerminalLoader.startAsDaemon(miraiConsoleImpl(Paths.get(".").toAbsolutePath()))
         MiraiConsole.apply {
             StateCommand.register()
-            subscribeAlways<BotOnlineEvent> {
-                bot.friends.getOrNull(1438159989L)?.let { friend ->
-                    println("开始尝试发送心跳")
-                    friend.launch {
-                        while (isActive) {
-                            friend.sendMessage(buildString {
-                                appendLine("存活！")
-                                tasks.toMap().forEach { (uid, info) ->
-                                    appendLine("$uid -> $info")
-                                }
-                            })
-                            delay((10).minutesToMillis)
-                        }
+            val block: (Friend) -> Job = { friend ->
+                friend.launch {
+                    while (isActive) {
+                        friend.sendMessage(buildString {
+                            appendLine("存活！")
+                            tasks.toMap().forEach { (uid, info) ->
+                                appendLine("$uid -> $info")
+                            }
+                        })
+                        delay((10).minutesToMillis)
                     }
                 }
             }
+            Bot.botInstances.forEach { bot -> bot.friends.getOrNull(1438159989L)?.let(block) }
+            subscribeAlways<BotOnlineEvent> { bot.friends.getOrNull(1438159989L)?.let(block) }
         }
-        MiraiConsoleTerminalLoader.parse(args, exitProcess = true)
-        MiraiConsoleTerminalLoader.startAsDaemon(miraiConsoleImpl(Paths.get(".").toAbsolutePath()))
         try {
             MiraiConsole.job.join()
         } catch (e: CancellationException) {
