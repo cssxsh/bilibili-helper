9e11334d0b9e26dc709d10c7a893bb4e05f9e341
cssxsh <cssxsh@gmail.com>
2020-12-10 23:35:24 +0800

    update: kotlin

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,9 +5,9 @@ object Versions {
     const val console = "1.0.1"
     const val autoService = "1.0-rc7"
     // kotlin
-    const val kotlin = "1.4.10"
+    const val kotlin = "1.4.20"
     const val serialization = "1.0-M1-1.4.0-rc"
-    const val ktor = "1.4.1"
+    const val ktor = "1.4.3"
     // other
     const val shadow = "6.1.0"
     const val junit = "5.6.2"
