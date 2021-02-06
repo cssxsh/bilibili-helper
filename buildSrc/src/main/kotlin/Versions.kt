f075d5f682a53cb6f3f327f492bef1086453f99e
cssxsh <cssxsh@gmail.com>
2021-02-06 12:50:50 +0800

    update: mirai

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,14 +1,14 @@
 
 object Versions {
     // mirai
-    const val core = "2.0.0"
-    const val console = "2.0.0"
-    const val autoService = "1.0-rc7"
+    const val core = "2.3.2"
+    const val console = "2.3.2"
     // kotlin
-    const val kotlin = "1.4.20"
+    const val kotlin = "1.4.30"
     const val serialization = "1.0-M1-1.4.0-rc"
-    const val ktor = "1.4.3"
+    const val ktor = "1.5.0"
     // other
+    const val autoService = "1.0-rc7"
     const val shadow = "6.1.0"
     const val junit = "5.6.2"
     const val selenium = "3.141.59"
