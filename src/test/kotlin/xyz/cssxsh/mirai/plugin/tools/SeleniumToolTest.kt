e192b21179769af49828b87d538fe6de7b19f7e9
cssxsh <cssxsh@gmail.com>
2021-06-10 19:03:15 +0800

    fix: KtorHttpClientFactory

--- src/test/kotlin/xyz/cssxsh/mirai/plugin/tools/SeleniumToolTest.kt
+++ src/test/kotlin/xyz/cssxsh/mirai/plugin/tools/SeleniumToolTest.kt
@@ -1,24 +1,16 @@
 package xyz.cssxsh.mirai.plugin.tools
 
-import io.github.karlatemp.mxlib.MxLib
-import io.github.karlatemp.mxlib.logger.NopLogger
-import io.github.karlatemp.mxlib.selenium.MxSelenium
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import org.openqa.selenium.remote.http.HttpClient
 import java.io.File
-import java.util.logging.Level
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class SeleniumToolTest {
     private val dir = File("./test")
 
     init {
-        MxLib.setLoggerFactory { name -> NopLogger(name) }
-        setSeleniumLogLevel(Level.ALL)
-        MxLib.setDataStorage(dir)
-        // System.setProperty("webdriver.http.factory", "ktor")
+        setupSelenium(dir)
     }
 
     private val list = listOf(
@@ -31,7 +23,7 @@ internal class SeleniumToolTest {
 
     @Test
     fun getScreenShot(): Unit = runBlocking {
-        val driver = MxSelenium.newDriver(null, DriverConsumer)
+        val driver = RemoteWebDriver()
         driver.get("https://t.bilibili.com/h5/dynamic/detail/508396365455813655")
         list.forEach { id ->
             driver.getScreenshot(dynamic(id)).let {
