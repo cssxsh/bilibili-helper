5e02b9620db704cfde34a84864c2f9fca804be8f
cssxsh <cssxsh@gmail.com>
2021-08-23 05:46:05 +0800

    build: 1.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "xyz.cssxsh.mirai.plugin"
-version = "1.1.1"
+version = "1.1.2"
 
 repositories {
     mavenLocal()
--- src/main/kotlin/xyz/cssxsh/mirai/plugin/BiliHelperPlugin.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/plugin/BiliHelperPlugin.kt
@@ -12,7 +12,7 @@ import xyz.cssxsh.mirai.plugin.data.*
 import xyz.cssxsh.mirai.plugin.tools.*
 
 object BiliHelperPlugin : KotlinPlugin(
-    JvmPluginDescription("xyz.cssxsh.mirai.plugin.bilibili-helper", "1.1.1") {
+    JvmPluginDescription("xyz.cssxsh.mirai.plugin.bilibili-helper", "1.1.2") {
         name("bilibili-helper")
         author("cssxsh")
     }
