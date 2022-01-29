5190f7f2187c7add53ff7315c047d1fad3ece57a
cssxsh <cssxsh@gmail.com>
2022-01-29 22:16:21 +0800

    fix: BiliListener

--- src/main/kotlin/xyz/cssxsh/mirai/plugin/BiliListener.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/plugin/BiliListener.kt
@@ -5,21 +5,20 @@ import net.mamoe.mirai.console.command.CommandSender.Companion.toCommandSender
 import net.mamoe.mirai.console.permission.PermissionService.Companion.testPermission
 import net.mamoe.mirai.utils.*
 import net.mamoe.mirai.event.*
+import net.mamoe.mirai.event.events.*
 import xyz.cssxsh.mirai.plugin.command.*
 
 internal object BiliListener : CoroutineScope by BiliHelperPlugin.childScope("BiliListener") {
     private val permission by BiliInfoCommand::permission
 
-    fun subscribe(): Unit = with(globalEventChannel()) {
-        subscribeMessages {
-            always {
-                if (permission.testPermission(toCommandSender()).not()) return@always
+    fun subscribe() {
+        globalEventChannel()
+            .filter { it is MessageEvent && permission.testPermission(it.toCommandSender()) }
+            .subscribeMessages {
                 for ((regex, replier) in UrlRepliers) {
-                    // regex findingReply replier
-                    replier.invoke(this, regex.find(it) ?: continue)
+                    regex findingReply replier
                 }
             }
-        }
     }
 
     fun stop() {
