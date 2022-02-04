4b21404d52b8ef26e0547327148670c565a09db2
cssxsh <cssxsh@gmail.com>
2022-02-04 14:37:41 +0800

    build: 1.4.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,16 @@ plugins {
     kotlin("jvm") version "1.6.0"
     kotlin("plugin.serialization") version "1.6.0"
 
-    id("net.mamoe.mirai-console") version "2.10.0-RC2"
+    id("net.mamoe.mirai-console") version "2.10.0"
     id("net.mamoe.maven-central-publish") version "0.7.0"
 }
 
 group = "xyz.cssxsh"
-version = "1.4.2"
+version = "1.4.3"
 
 repositories {
     mavenLocal()
-    maven(url = "https://maven.aliyun.com/repository/central")
     mavenCentral()
-    maven(url = "https://maven.aliyun.com/repository/gradle-plugin")
     gradlePluginPortal()
 }
 
@@ -42,11 +40,11 @@ dependencies {
         exclude(group = "io.ktor", module = "ktor-client-core-jvm")
     }
     compileOnly("xyz.cssxsh.mirai:mirai-selenium-plugin:2.0.6")
-    compileOnly("net.mamoe:mirai-core:2.10.0-RC2")
-    compileOnly("net.mamoe:mirai-core-utils:2.10.0-RC2")
+    compileOnly("net.mamoe:mirai-core:2.10.0")
+    compileOnly("net.mamoe:mirai-core-utils:2.10.0")
 
     testImplementation(kotlin("test", "1.6.0"))
-    testImplementation("org.slf4j:slf4j-simple:1.7.33")
+    testImplementation("org.slf4j:slf4j-simple:1.7.35")
 }
 
 mirai {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,7 @@
 pluginManagement {
     repositories {
         mavenLocal()
-        maven(url = "https://maven.aliyun.com/repository/central")
         mavenCentral()
-        maven(url = "https://maven.aliyun.com/repository/gradle-plugin")
         gradlePluginPortal()
     }
 }
