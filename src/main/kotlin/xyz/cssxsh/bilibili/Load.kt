d5fa993c5a780205d8934858b303019648f9b98a
cssxsh <cssxsh@gmail.com>
2022-02-07 20:43:45 +0800

    fix: url regex

--- src/main/kotlin/xyz/cssxsh/bilibili/Load.kt
+++ src/main/kotlin/xyz/cssxsh/bilibili/Load.kt
@@ -5,22 +5,6 @@ import kotlin.properties.*
 
 internal fun timestamp(sec: Long) = OffsetDateTime.ofInstant(Instant.ofEpochSecond(sec), ZoneOffset.systemDefault())
 
-val VIDEO_REGEX = """(?i)(?<!\w)(av\d+|BV[0-9A-z]{8,12})""".toRegex()
-
-val DYNAMIC_REGEX = """(?<=t\.bilibili\.com/(h5/dynamic/detail/)?|m\.bilibili\.com/dynamic/)(\d+)""".toRegex()
-
-val ROOM_REGEX = """(?<=live\.bilibili\.com/)(\d+)""".toRegex()
-
-val SHORT_LINK_REGEX = """(?<=b23\.tv\\?/)[0-9A-z]+""".toRegex()
-
-val SPACE_REGEX = """(?<=space\.bilibili\.com/)(\d+)""".toRegex()
-
-val SEASON_REGEX = """(?<=bilibili\.com/bangumi/play/ss)(\d+)""".toRegex()
-
-val EPISODE_REGEX = """(?<=bilibili\.com/bangumi/play/ep)(\d+)""".toRegex()
-
-val MEDIA_REGEX = """(?<=bilibili\.com/bangumi/media/md)(\d+)""".toRegex()
-
 /**
  * 2017-07-01 00:00:00
  */
--- src/main/kotlin/xyz/cssxsh/mirai/plugin/BiliBuilder.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/plugin/BiliBuilder.kt
@@ -8,7 +8,6 @@ import net.mamoe.mirai.event.events.*
 import net.mamoe.mirai.message.data.*
 import net.mamoe.mirai.message.data.MessageSource.Key.quote
 import net.mamoe.mirai.utils.*
-import xyz.cssxsh.bilibili.*
 import xyz.cssxsh.bilibili.api.*
 import xyz.cssxsh.bilibili.data.*
 
@@ -29,22 +28,28 @@ internal suspend fun SearchResult<*>.toMessage(contact: Contact): Message {
 
 typealias MessageReplier = suspend MessageEvent.(MatchResult) -> Any?
 
+internal val DYNAMIC_REGEX = """(?<=t\.bilibili\.com/(h5/dynamic/detail/)?|m\.bilibili\.com/dynamic/)(\d+)""".toRegex()
+
 internal val DynamicReplier: MessageReplier = replier@{ result ->
     logger.info { "${sender.render()} 匹配Dynamic(${result.value})" }
     try {
-        message.quote() + client.getDynamicInfo(result.value.toLong()).dynamic.content(subject)
+        val (id) = result.destructured
+        message.quote() + client.getDynamicInfo(id.toLong()).dynamic.content(subject)
     } catch (e: Throwable) {
         logger.warning({ "构建Dynamic(${result.value})信息失败" }, e)
         e.message
     }
 }
 
+internal val VIDEO_REGEX = """(?i)(?<!\w)(?:av(\d+)|(BV[0-9A-z]{8,12}))""".toRegex()
+
 internal val VideoReplier: MessageReplier = replier@{ result ->
     logger.info { "${sender.render()} 匹配Video(${result.value})" }
     try {
+        val (id) = result.destructured
         message.quote() + when (result.value.first()) {
-            'B', 'b' -> client.getVideoInfo(result.value)
-            'A', 'a' -> client.getVideoInfo(result.value.substring(2).toLong())
+            'B', 'b' -> client.getVideoInfo(id)
+            'A', 'a' -> client.getVideoInfo(id.toLong())
             else -> throw IllegalArgumentException("未知视频ID(${result.value})")
         }.content(contact = subject)
     } catch (e: Throwable) {
@@ -53,52 +58,80 @@ internal val VideoReplier: MessageReplier = replier@{ result ->
     }
 }
 
+internal val ROOM_REGEX = """(?<=live\.bilibili\.com/(h5/)?)(\d+)""".toRegex()
+
 internal val RoomReplier: MessageReplier = replier@{ result ->
     logger.info { "${sender.render()} 匹配Room(${result.value})" }
     try {
-        message.quote() + client.getRoomInfo(result.value.toLong()).content(subject)
+        val (id) = result.destructured
+        message.quote() + client.getRoomInfo(id.toLong()).content(subject)
     } catch (e: Throwable) {
         logger.warning({ "构建Room(${result.value})信息失败" }, e)
         e.message
     }
 }
 
+internal val SPACE_REGEX = """(?<=space\.bilibili\.com/|bilibili\.com/space/)(\d+)""".toRegex()
+
 internal val SpaceReplier: MessageReplier = replier@{ result ->
     logger.info { "${sender.render()} 匹配User(${result.value})" }
     try {
-        message.quote() + client.getUserInfo(result.value.toLong()).content(subject)
+        val (id) = result.destructured
+        message.quote() + client.getUserInfo(id.toLong()).content(subject)
     } catch (e: Throwable) {
         logger.warning({ "构建User(${result.value})信息失败" }, e)
         e.message
     }
 }
 
+internal val SEASON_REGEX = """(?i)(?<!\w)ss(\d+)""".toRegex()
+
 internal val SeasonReplier: MessageReplier = replier@{ result ->
     logger.info { "${sender.render()} 匹配Season(${result.value})" }
     try {
-        message.quote() + client.getSeasonInfo(result.value.toLong()).content(subject)
+        val (id) = result.destructured
+        message.quote() + client.getSeasonInfo(id.toLong()).content(subject)
     } catch (e: Throwable) {
         logger.warning({ "构建Season(${result.value})信息失败" }, e)
         e.message
     }
 }
 
+internal val EPISODE_REGEX = """(?i)(?<!\w)eq(\d+)""".toRegex()
+
 internal val EpisodeReplier: MessageReplier = replier@{ result ->
     logger.info { "${sender.render()} 匹配Episode(${result.value})" }
     try {
-        message.quote() + client.getEpisodeInfo(result.value.toLong()).content(subject)
+        val (id) = result.destructured
+        message.quote() + client.getEpisodeInfo(id.toLong()).content(subject)
     } catch (e: Throwable) {
         logger.warning({ "构建Episode(${result.value})信息失败" }, e)
         e.message
     }
 }
 
+internal val MEDIA_REGEX = """(?i)(?<!\w)md(\d+)""".toRegex()
+
 internal val MediaReplier: MessageReplier = replier@{ result ->
     logger.info { "${sender.render()} 匹配Media(${result.value})" }
     try {
-        message.quote() + client.getSeasonMedia(result.value.toLong()).media.content(subject)
+        val (id) = result.destructured
+        message.quote() + client.getSeasonMedia(id.toLong()).media.content(subject)
     } catch (e: Throwable) {
         logger.warning({ "构建Media(${result.value})信息失败" }, e)
+        e.message
+    }
+}
+
+internal val ARTICLE_REGEX = """(?i)(?<!\w)cv(\d+)|(?<=bilibili\.com/read/mobile\?id=)(\d+)""".toRegex()
+
+internal val ArticleReplier: MessageReplier = replier@{ result ->
+    logger.info { "${sender.render()} 匹配Article(${result.value})" }
+    try {
+        val (id) = result.destructured
+        message.quote() + client.getArticleInfo(id.toLong()).last.content(subject)
+    } catch (e: Throwable) {
+        logger.warning({ "构建Article(${result.value})信息失败" }, e)
         e.message
     }
 }
@@ -112,6 +145,17 @@ private suspend fun Url.location(): String? {
     }.headers[HttpHeaders.Location]
 }
 
+internal val SHORT_LINK_REGEX = """(?<=b23\.tv\\?/)[0-9A-z]+""".toRegex()
+
+internal val ShortLinkReplier: MessageReplier = replier@{ result ->
+    logger.info { "${sender.render()} 匹配SHORT_LINK(${result.value}) 尝试跳转" }
+    val location = Url("https://b23.tv/${result.value}").location() ?: return@replier null
+    for ((regex, replier) in UrlRepliers) {
+        val new = regex.find(location) ?: continue
+        return@replier replier(new)
+    }
+}
+
 internal val UrlRepliers by lazy {
     mapOf(
         DYNAMIC_REGEX to DynamicReplier,
@@ -121,15 +165,7 @@ internal val UrlRepliers by lazy {
         SEASON_REGEX to SeasonReplier,
         EPISODE_REGEX to EpisodeReplier,
         MEDIA_REGEX to MediaReplier,
+        ARTICLE_REGEX to ArticleReplier,
         SHORT_LINK_REGEX to ShortLinkReplier
     )
-}
-
-internal val ShortLinkReplier: MessageReplier = replier@{ result ->
-    logger.info { "${sender.render()} 匹配SHORT_LINK(${result.value}) 尝试跳转" }
-    val location = Url("https://b23.tv/${result.value}").location() ?: return@replier null
-    for ((regex, replier) in UrlRepliers) {
-        val new = regex.find(location) ?: continue
-        return@replier replier(new)
-    }
 }
