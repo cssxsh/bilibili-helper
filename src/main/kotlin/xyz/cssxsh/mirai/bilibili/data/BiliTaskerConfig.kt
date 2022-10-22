1971a0eb8a183014b6e717cbc9ebdda39754a443
cssxsh <cssxsh@gmail.com>
2022-10-23 04:05:59 +0800

    fix: permittee map

--- src/main/kotlin/xyz/cssxsh/mirai/bilibili/data/BiliTaskerConfig.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/bilibili/data/BiliTaskerConfig.kt
@@ -1,10 +1,18 @@
 package xyz.cssxsh.mirai.bilibili.data
 
 import net.mamoe.mirai.console.data.*
-import net.mamoe.mirai.console.data.PluginDataExtensions.mapKeys
 import net.mamoe.mirai.console.permission.*
 
 object BiliTaskerConfig : AutoSavePluginConfig("BiliTaskerConfig") {
+
+    private fun <V> permittee(): SerializerAwareValue<MutableMap<PermitteeId, V>> {
+        return with(PluginDataExtensions) {
+            value<MutableMap<String, V>>().mapKeys(
+                oldToNew = AbstractPermitteeId.Companion::parseFromString,
+                newToOld = PermitteeId::asString
+            )
+        }
+    }
 
     @ValueName("date_time_pattern")
     var pattern: String by value("ISO_OFFSET_DATE_TIME")
@@ -16,12 +24,10 @@ object BiliTaskerConfig : AutoSavePluginConfig("BiliTaskerConfig") {
     val dynamicForbidRegexes: MutableSet<String> by value()
 
     @ValueName("dynamic_sleep")
-    val dynamicSleep: MutableMap<PermitteeId, BiliInterval> by value<MutableMap<String, BiliInterval>>()
-        .mapKeys(oldToNew = AbstractPermitteeId.Companion::parseFromString, newToOld = PermitteeId::asString)
+    val dynamicSleep: MutableMap<PermitteeId, BiliInterval> by permittee()
 
     @ValueName("dynamic_at")
-    val dynamicAt: MutableMap<PermitteeId, BiliInterval> by value<MutableMap<String, BiliInterval>>()
-        .mapKeys(oldToNew = AbstractPermitteeId.Companion::parseFromString, newToOld = PermitteeId::asString)
+    val dynamicAt: MutableMap<PermitteeId, BiliInterval> by permittee()
 
     @ValueName("video_forbid_type")
     val videoForbidType: MutableSet<Int> by value()
@@ -39,26 +45,20 @@ object BiliTaskerConfig : AutoSavePluginConfig("BiliTaskerConfig") {
     var videoForbidPlayback: Boolean by value()
 
     @ValueName("video_sleep")
-    val videoSleep: MutableMap<PermitteeId, BiliInterval> by value<MutableMap<String, BiliInterval>>()
-        .mapKeys(oldToNew = AbstractPermitteeId.Companion::parseFromString, newToOld = PermitteeId::asString)
+    val videoSleep: MutableMap<PermitteeId, BiliInterval> by permittee()
 
     @ValueName("video_at")
-    val videoAt: MutableMap<PermitteeId, BiliInterval> by value<MutableMap<String, BiliInterval>>()
-        .mapKeys(oldToNew = AbstractPermitteeId.Companion::parseFromString, newToOld = PermitteeId::asString)
+    val videoAt: MutableMap<PermitteeId, BiliInterval> by permittee()
 
     @ValueName("season_sleep")
-    val seasonSleep: MutableMap<PermitteeId, BiliInterval> by value<MutableMap<String, BiliInterval>>()
-        .mapKeys(oldToNew = AbstractPermitteeId.Companion::parseFromString, newToOld = PermitteeId::asString)
+    val seasonSleep: MutableMap<PermitteeId, BiliInterval> by permittee()
 
     @ValueName("season_at")
-    val seasonAt: MutableMap<PermitteeId, BiliInterval> by value<MutableMap<String, BiliInterval>>()
-        .mapKeys(oldToNew = AbstractPermitteeId.Companion::parseFromString, newToOld = PermitteeId::asString)
+    val seasonAt: MutableMap<PermitteeId, BiliInterval> by permittee()
 
     @ValueName("live_sleep")
-    val liveSleep: MutableMap<PermitteeId, BiliInterval> by value<MutableMap<String, BiliInterval>>()
-        .mapKeys(oldToNew = AbstractPermitteeId.Companion::parseFromString, newToOld = PermitteeId::asString)
+    val liveSleep: MutableMap<PermitteeId, BiliInterval> by permittee()
 
     @ValueName("live_at")
-    val liveAt: MutableMap<PermitteeId, BiliInterval> by value<MutableMap<String, BiliInterval>>()
-        .mapKeys(oldToNew = AbstractPermitteeId.Companion::parseFromString, newToOld = PermitteeId::asString)
+    val liveAt: MutableMap<PermitteeId, BiliInterval> by permittee()
 }
