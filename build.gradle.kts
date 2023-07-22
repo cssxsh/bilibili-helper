43a4420ab4725ebde7060d96cec1b5b742ec072d
cssxsh <cssxsh@gmail.com>
2023-07-23 00:02:14 +0800

    update: dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "1.7.22"
-    kotlin("plugin.serialization") version "1.7.22"
+    kotlin("jvm") version "1.8.10"
+    kotlin("plugin.serialization") version "1.8.10"
 
-    id("net.mamoe.mirai-console") version "2.14.0"
+    id("net.mamoe.mirai-console") version "2.15.0"
     id("me.him188.maven-central-publish") version "1.0.0-dev-3"
 }
 
@@ -28,16 +28,16 @@ mavenCentralPublish {
 dependencies {
     implementation("com.cronutils:cron-utils:9.2.1")
     implementation("com.google.zxing:javase:3.5.1")
-    compileOnly("xyz.cssxsh.mirai:mirai-selenium-plugin:2.3.0")
+    compileOnly("xyz.cssxsh.mirai:mirai-selenium-plugin:2.4.0")
     testImplementation(kotlin("test"))
-    testImplementation("xyz.cssxsh.mirai:mirai-selenium-plugin:2.3.0")
+    testImplementation("xyz.cssxsh.mirai:mirai-selenium-plugin:2.4.0")
     //
-    implementation(platform("net.mamoe:mirai-bom:2.14.0"))
+    implementation(platform("net.mamoe:mirai-bom:2.15.0"))
     compileOnly("net.mamoe:mirai-core")
     compileOnly("net.mamoe:mirai-console-compiler-common")
     testImplementation("net.mamoe:mirai-logging-slf4j")
     //
-    implementation(platform("io.ktor:ktor-bom:2.1.3"))
+    implementation(platform("io.ktor:ktor-bom:2.2.4"))
     implementation("io.ktor:ktor-client-okhttp")
     implementation("io.ktor:ktor-client-encoding")
     implementation("io.ktor:ktor-client-content-negotiation")
