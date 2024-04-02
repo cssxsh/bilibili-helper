9c285efe423fba73ac8187d955513a2e4df5b0d2
cssxsh <cssxsh@gmail.com>
2024-04-02 12:19:08 +0800

    fix: VideoTest

--- src/test/kotlin/xyz/cssxsh/bilibili/VideoTest.kt
+++ src/test/kotlin/xyz/cssxsh/bilibili/VideoTest.kt
@@ -7,6 +7,7 @@ import kotlin.test.*
 internal class VideoTest : ApiTest() {
 
     @Test
+    @org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable(named = "CI", matches = "true")
     fun `user video`(): Unit = runBlocking {
         val result = client.getVideos(uid = 26798384L)
         assertFalse(result.list.videos.isEmpty())
